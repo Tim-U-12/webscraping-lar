d4ad8184fe84fdc5332bcb607ce6b560a2bf54cd
Tim <tim.a.ung.12@gmail.com>
2023-10-04 10:48:05 +1100

    created MainPage class and tidied things up

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://lil-alchemist.fandom.com/wiki/Category:Gold"
-
 response = requests.get(url)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+if __name__ == "__main__":
 
-    specific_class_elements = soup.findAll(class_='category-page__member-link')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        specific_class_elements = soup.findAll(class_='category-page__member-link')
 
-    if specific_class_elements:
-        # Loop through each element and print its text
-        for element in specific_class_elements:
-            print(f"Text inside element with specific class: {element}")
+        if specific_class_elements:
+            # Loop through each element and print its text
+            for container in specific_class_elements:
+                print(container['href'])
+                #for link in container.findAll('a',href=True):
+                #    print(link['href'])
+        else:
+            print("Specified class not found in the page.")
+
     else:
-        print("Specified class not found in the page.")
-
-else:
-    print(f"Failed to retrieve the page: {response.status_code}")
-    
+        print(f"Failed to retrieve the page: {response.status_code}")
+        
