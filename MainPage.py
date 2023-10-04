d700e4075a3f08d747a355edcadf25a2001f401d
Tim <tim.a.ung.12@gmail.com>
2023-10-04 11:21:59 +1100

    added a method to extract all of the href from within a specified container

--- MainPage.py
+++ MainPage.py
@@ -14,7 +14,15 @@ class MainPage():
     def search_class(self, class_name="category-page__members") -> list:
         return self.soup.find_all(class_=class_name)
 
+    def extract_links(self, container) -> list:
+        links = []
+        for element in container:
+            for link in element.findAll('a',href=True):
+                links.append(link['href'])
+        return links
+
 
 url = "https://lil-alchemist.fandom.com/wiki/Category:Gold"
 x = MainPage(url)
-print(x.search_class())
+class_name = x.search_class()
+print(x.extract_links(class_name))
