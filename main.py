63690a0efd61ab68d982ba92a1d038a5521b1dc8
Tim <tim.a.ung.12@gmail.com>
2023-10-04 11:22:30 +1100

    cleanup

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from bs4 import BeautifulSoup
 url = "https://lil-alchemist.fandom.com/wiki/Category:Gold"
 response = requests.get(url)
 
+
 if __name__ == "__main__":
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-     
-        for link in soup.findAll('a',href=True):
-            print(link['href'])
+
+        specific_class_elements = soup.findAll(class_='category-page__member-link')
+
+        if specific_class_elements:
+            # Loop through each element and print its text
+            for container in specific_class_elements:
+                print(container['href'])
+                #for link in container.findAll('a',href=True):
+                #    print(link['href'])
+        else:
+            print("Specified class not found in the page.")
 
     else:
         print(f"Failed to retrieve the page: {response.status_code}")
